beacc18560dc3704bfbeb4454f22f5065f19649a
guolei987 <46949687+guolei987@users.noreply.github.com>
2022-04-27 21:31:28 +0800

    second commit

---
+++ src/GitTest.java
@@ -0,0 +1,9 @@
+/**
+ * @auther guolei
+ * @create 2022-4-27 21:26
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("测试push到GitHub上");
+    }
+}
